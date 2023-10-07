9d4a98a777b479092d77e0cca2d94b3be021b042
Svitlana <wsv3ta@gmail.com>
2023-10-07 13:24:00 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
 
+import Player from '@vimeo/player';
+import _ from 'lodash';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  _.throttle(data => {
+    console.log(data.seconds);
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
