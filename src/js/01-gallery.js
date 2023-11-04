e2eca0d456e7686e2f529a7ac825c7c7588b2ce9
Svitlana <wsv3ta@gmail.com>
2023-11-04 13:46:52 +0100

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,13 +4,15 @@ import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 const gallery = document.querySelector(".gallery");
+let markup = "";
 for (const { preview, original, description } of galleryItems) {
-  gallery.innerHTML += `<li class="gallery__item">
+  markup += `<li class="gallery__item">
     <a class="gallery__link" href="${original}">
     <img class="gallery__image" src="${preview}" alt="${description}" />
     </a>
     </li>`;
 }
+gallery.innerHTML = markup;
 
 const lightbox = new SimpleLightbox(".gallery a", {
   captionsData: "alt",
